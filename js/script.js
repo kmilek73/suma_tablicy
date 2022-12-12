bfea0a128e1cb6a29e19158b03fe05e879b1ab1f
kmilek <kmilek73@gmail.com>
2022-12-12 10:53:50 +0100

    rozwiazanie właściwe z mnożeniem

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 let numbers = [1,2,3,4,5,6];
 let dlugoscTablicy = numbers.length;
 let suma=0;
+let iloczyn=1;
 
 
 function sumaWszystkich(){
+
 for (let i=0; i<dlugoscTablicy; i++){
     suma = suma + numbers[i] ;
+    iloczyn = iloczyn * numbers[i];
     }
     console.log(suma);
+    console.log(iloczyn);
 }
 
 sumaWszystkich(numbers);
