c42bef2ad73f956a0b23fc00d8df92cde1aa10aa
kmilek <kmilek73@gmail.com>
2022-12-13 11:07:00 +0100

    rozwiązanie po poprawkach

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let numbers = [1,2,3,4,5,6];
+
+
+
+function sumaWszystkich(numbers){
 let dlugoscTablicy = numbers.length;
 let suma=0;
 let iloczyn=1;
-
-
-function sumaWszystkich(){
-
 for (let i=0; i<dlugoscTablicy; i++){
     suma = suma + numbers[i] ;
     iloczyn = iloczyn * numbers[i];
